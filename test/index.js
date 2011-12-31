9dec99beaf50fdcc1c5a86f9cc8f97116eae3b40
Elijah Insua <tmpvar@gmail.com>
2011-12-31 16:20:58 -0700

    convert remaining tests to mocha

--- test/index.js
+++ test/index.js
@@ -122,7 +122,198 @@ describe('Thing.js', function() {
         tm.remove('removal-test');
         ok(!a.one)
         ok(!a.two);
+      });
+    });
+  });
+
+  describe('Thing', function() {
+    describe('#trait', function() {
+      it('should allow a user to register a trait (function)', function() {
+        Thing.trait('test_trait', function() {});
+        ok(Thing.traits.test_trait);
+      });
+
+      it('should allow a user to register a trait (object)', function() {
+        Thing.trait('test_trait2', {});
+        ok(Thing.traits.test_trait2);
+      });
+
+      it('should not allow a user to register a falsy trait', function() {
+        Thing.trait('invalid', false);
+        ok(!Thing.traits.invalid);
+      });
+
+      it('should be able to define initialization step(s)', function() {
+        Thing.trait('trait1', function(proto) {
+          proto.init(function(options) {
+            this.options = options
+          });
+        });
+
+        Thing.trait('trait2', function(proto) {
+          proto.init(function(options) {
+            this.b = true;
+          });
+        });
+
+        var thing = Thing.create(['trait1', 'trait2'], { a : true });
+        ok(thing.options.a, 'options should be appended');
+        ok(thing.b, 'b should be appended');
+      });
+
+      it('should copy the initialization list so classes do not step on each other', function() {
+        Thing.trait('first', function(proto) {
+          proto.init(function() {
+            this.first = true;
+          });
+        });
+
+        Thing.trait('second', function(proto) {
+          proto.init(function() {
+            this.second = true;
+          });
+        });
+
+        var Both = Thing.createClass(['first', 'second']);
+        var First = Thing.createClass(['first']);
+        var Second = Thing.createClass(['second']);
+
+        var both = new Both();
+        ok(both.first && both.second, 'should contain both first and second properties');
+
+        var first = new First();
+        ok(first.first, 'first has first');
+        ok(!first.second, 'first does not have second');
+
+        var second = new Second();
+        ok(!second.first, 'second does not have first');
+        ok(second.second, 'second has second');
+      });
+    });
+
+    describe('#create', function() {
+      it('should create things with traits', function() {
+        Thing.trait('first', { first : true });
+        Thing.trait('second', { second : true });
+        var instance = Thing.create(['first', 'second']);
+
+        ok(instance.first, 'first trait was applied');
+        ok(instance.second, 'second trait was applied');
       })
+    });
+
+    describe('#createClass', function() {
+      it('should create a ctor+prototype that contains expected traits', function() {
+        Thing.trait('create-class-test', function(proto) {
+          proto.createClassTest = true;
+        });
+
+        var p = Thing.createClass(['create-class-test']);
+        ok(p.prototype.createClassTest);
+      });
+
+      it('should handle composite traits like a boss', function() {
+        Thing.trait('child', {
+          child : true
+        });
+
+        Thing.trait('child2', {
+          child2 : true
+        });
+
+        Thing.trait('parent', ['child', 'child2'], {
+          parent : true
+        });
+
+        var Parent = Thing.createClass(['parent']);
+        var p = new Parent();
+        ok(p.child, 'child trait was applied');
+        ok(p.child2, 'child2 trait was applied');
+        ok(p.parent, 'parent trait was applied');
+      });
+
+      it('should instantiate and expose a traits manager on the class', function() {
+        Thing.trait('a trait', {});
+        var Class = Thing.createClass(['a trait']);
+        ok(Class.traits.has('a trait'), 'it has a trait');
+      });
+
+      it('should be extendable via the traits manager', function() {
+        Thing.trait('base', { base : true });
+        Thing.trait('extension', { extension : true });
+
+        var Class = Thing.createClass(['base']);
+        Class.traits.add('extension');
+
+        var instance = new Class();
+        ok(instance.base, 'base was defined');
+        ok(instance.extension, 'extension was defined');
+      });
+
+      it('should be managable via the exposed trait manager', function() {
+        Thing.trait('trait', {});
+        var Class = Thing.createClass(['trait']);
+        ok(Class.traits.has('trait'), 'it has a trait');
+
+        Class.traits.remove('trait');
+        ok(Class.traits.has('trait') === false);
+      });
+    });
+
+    describe('#meta', function() {
+      var t;
+      beforeEach(function() {
+        var Ctor = Thing.createClass();
+        t = new Ctor();
+      });
+
+      it('should return null when getting a non-existant key', function() {
+        ok(null === t.meta('undefined'));
+      });
+
+      it('should return the current value when set', function() {
+        ok(1 === t.meta('a', 1));
+      });
+
+      it('should act as a getter/setter', function() {
+        t.meta('a', 1);
+        ok(1 === t.meta('a'));
+      });
+
+      it('should allow metadata to be frozen', function() {
+        t.meta('a', 1, { freeze : true });
+        ok(1 === t.meta('a', 2));
+        ok(1 === t.meta('a'));
+      });
+    });
+
+    describe('constructor', function() {
+      it('should generate a locally unique id', function() {
+        var t = Thing.create(), t2 = Thing.create();
+        ok(t.meta('id') < t2.meta('id'));
+      });
+
+      it('should accept an id', function() {
+        var t = Thing.create([], {
+          id : 'abc123'
+        });
+
+        ok(t.meta('id') === 'abc123');
+      });
+    });
+
+    describe('#create', function() {
+      it('should create a traitless Things', function() {
+        ok(Thing.create().meta);
+      });
+
+      it('should apply traits, instiate, and return a new Thing', function() {
+        Thing.trait('some-trait', function(proto) {
+          proto.used = true
+        });
+
+        ok(Thing.create(['some-trait']).used);
+      });
     });
   });
 
@@ -144,166 +335,3 @@ describe('Thing.js', function() {
 
   });
 });
-
-var tests = {
-
-  'trait creation' : function(t) {
-    Thing.trait('test_trait', function() {});
-    t.ok(Thing.traits.test_trait, 'test trait exists');
-    t.done();
-  },
-
-  'composite traits' : function(t) {
-    Thing.trait('child', {
-      child : true
-    });
-
-    Thing.trait('child2', {
-      child2 : true
-    });
-
-    Thing.trait('parent', ['child', 'child2'], {
-      parent : true
-    });
-
-    var Parent = Thing.class(['parent']);
-    var p = new Parent();
-    t.ok(p.child, 'child trait was applied');
-    t.ok(p.child2, 'child2 trait was applied');
-    t.ok(p.parent, 'parent trait was applied');
-    t.done();
-  },
-
-  'basic thing creation' : function(t) {
-    var baseThing = Thing.create();
-    t.ok(baseThing.meta, 'this is an empty thing');
-    t.done();
-  },
-
-  'thing creation with traits' : function(t) {
-    Thing.trait('first', { first : true });
-    Thing.trait('second', { second : true });
-    var instance = Thing.create(['first', 'second']);
-
-    t.ok(instance.first, 'first trait was applied');
-    t.ok(instance.second, 'second trait was applied');
-    t.done();
-  },
-
-  'expose traits on class' : function(t) {
-    Thing.trait('a trait', {});
-    var Class = Thing.class(['a trait']);
-    t.ok(Class.traits.has('a trait'), 'it has a trait');
-    t.done()
-  },
-
-  'init list should be unique to classes of objects' : function(t) {
-    Thing.trait('first', function(proto) {
-      proto.init(function() {
-        this.first = true;
-      });
-    });
-
-    Thing.trait('second', function(proto) {
-      proto.init(function() {
-        this.second = true;
-      });
-    });
-
-    var Both = Thing.class(['first', 'second']);
-    var First = Thing.class(['first']);
-    var Second = Thing.class(['second']);
-
-    var both = new Both();
-    t.ok(both.first && both.second, 'should contain both first and second properties');
-
-    var first = new First();
-    t.ok(first.first, 'first has first');
-    t.ok(!first.second, 'first does not have second');
-
-    var second = new Second();
-    t.ok(!second.first, 'second does not have first');
-    t.ok(second.second, 'second has second');
-    t.done();
-
-  },
-
-  'thing initialization' : function(t) {
-    Thing.trait('trait1', function(proto) {
-      proto.init(function(options) {
-        this.options = options
-      });
-    });
-
-    Thing.trait('trait2', function(proto) {
-      proto.init(function(options) {
-        this.b = true;
-      });
-    });
-
-    var thing = Thing.create(['trait1', 'trait2'], { a : true });
-    t.ok(thing.options.a, 'options should be appended');
-    t.ok(thing.b, 'b should be appended');
-    t.done();
-  },
-
-  'remove traits before creation' : function(t) {
-    Thing.trait('trait', {});
-    var Class = Thing.class(['trait']);
-    t.ok(Class.traits.has('trait'), 'it has a trait');
-    Class.traits.remove('trait');
-    t.equals(Class.traits.has('trait'), false, 'it no longer has a trait');
-    t.done();
-  },
-
-  'class extension' : function(t) {
-    Thing.trait('base', { base : true });
-    Thing.trait('extension', { extension : true });
-
-    var Class = Thing.class(['base']);
-    Class.traits.add('extension');
-
-    var instance = new Class();
-    t.ok(instance.base, 'base was defined');
-    t.ok(instance.extension, 'extension was defined');
-
-    t.done();
-  },
-
-  'class creation' : function(t) {
-    Thing.trait('hello', {
-      hello : function() { return 'world'; }
-    });
-
-    Thing.trait('bye', function(proto) {
-      proto.bye = true;
-    });
-
-    var Hello = Thing.class(['hello', 'bye']);
-    var instance = new Hello();
-    t.equals(instance.hello(), 'world');
-    t.equals(instance.bye, true);
-    t.done();
-  },
-
-  'metadata' : function(t) {
-    var Obj = Thing.class();
-    var obj = new Obj();
-
-    t.equals(obj.meta('a', 1), 1);
-    t.equals(obj.meta('a'), 1);
-
-    t.done();
-  },
-
-  'metadata locking' : function(t) {
-    var Obj = Thing.class();
-    var obj = new Obj();
-
-    t.equals(obj.meta('a', 1, { freeze : true }), 1);
-    t.equals(obj.meta('a', 2), 1);
-    t.equals(obj.meta('a'), 1);
-
-    t.done();
-  }
-};
